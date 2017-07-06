5bb6a7b96a4e8bdda6b6f6dbdfce13bac89576fe
sanjeev <adminupd@gmail.com>
2017-07-06 18:29:03 +0530

    formatted

--- entertainment_center_v.1.py
+++ entertainment_center_v.1.py
@@ -1,18 +1,34 @@
+# coding=utf-8
+
+"""
+Module that uses the content of media.py to define class movie
+"""
 import media
 import requests
 import json
 import fresh_tomatoes
-#print (media.Movie.__name__)
-#print (media.Movie.__module__)
-#print (media.Movie.__doc__)
-# Constants for disocovery url and Image url as well as API key ( Before running this python code make sure to replace the API key with YOUR API KEY)
+
+"""
+ Constants for disocovery url and Image url as well as API key
+ Before running this python code make sure to replace the API key with YOUR API KEY)  # noqa
+ Discover URL is By popularity
+
+"""
 IMAGE_URL = "https://image.tmdb.org/t/p/w500"
-DISCOVER_URL = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc?&api_key=" #By popularity
-#DISCOVER_URL = "http://api.themoviedb.org/3/discover/movie?primary_release_year=2011&sort_by=vote_average.desc&api_key=" #By release Year
+DISCOVER_URL = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc?&api_key="  # noqa
 API_KEY = '3ffde410d62fa9617bb816fa0215335d'
 VIDEO_URL = "https://www.youtube.com/watch?v="
 
+
 def make_video_url(movie_id, api_key):
+
+    """
+    get Video link from themoviedb database
+
+    :param movie_id:
+    :param api_key:
+    :return:
+    """
 
     MOVIE_URL = "https://api.themoviedb.org/3/movie/"
     LANG = "&language=en-US"
@@ -21,29 +37,34 @@ def make_video_url(movie_id, api_key):
                               "/videos?api_key=" + api_key + LANG)
     videos_json = json.loads(connection.text)
     connection.close()
-    #print videos_json
+
     if connection.status_code != 200:
-        return VIDEO_URL + '5PSNL1qE6VY'  # constant in case issue is found with connection....
+        # constant in case issue is found with connection....
+        return VIDEO_URL + '5PSNL1qE6VY'
     else:
-       if len(videos_json['results']) == 0:
-          return VIDEO_URL + '5PSNL1qE6VY' # constant in case no video is found for given movie....I have checked many movies do not have video urls.So handled it
-       else:
-          return VIDEO_URL + videos_json['results'][0]['key'] # If all well we get aa video url for all movie based on discovery or discovery by year
+        if len(videos_json['results']) == 0:
+            # constant in case no video is found for given movie....
+            return VIDEO_URL + '5PSNL1qE6VY'
+        else:
+            # If all well we get aa video url for all movie
+            # based on discovery or discovery by year
+            return VIDEO_URL + videos_json['results'][0]['key']
 
 movies = []
 connection = requests.get(DISCOVER_URL + API_KEY)
 movies_json = json.loads(connection.text)
 connection.close()
-#popularity = movies_json['results'][0]
-#print popularity
 for rows in movies_json['results']:
-    title = rows['title'].encode('utf-8') #encodeing was required to deal with special character coming as part of movie data
-    story_line = rows['overview'].encode('utf-8') #encodeing was required to deal with special character coming as part of movie data
+    # encodeing was required to deal with special characters
+    # coming as part of movie data
+    title = rows['title'].encode('utf-8')
+    story_line = rows['overview'].encode('utf-8')
     img_url = IMAGE_URL + str(rows['poster_path'])
     movie_id = rows['id']
     v_url = make_video_url(movie_id, API_KEY)
-    #print v_url
-    movies.append(media.Movie(title, story_line, img_url, v_url)) # create and append movie insatnce
+
+    # create and append movie insatnce
+    movies.append(media.Movie(title, story_line, img_url, v_url))
 
 # Generate Web Page
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,11 +1,34 @@
-import webbrowser
-class Movie():
-    """ This class provides a way to store movie related information"""
-    VALID_RATINGS=["G","PG","PG-13","R","A"]
-    def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
-        self.title=movie_title
-        self.storyline=movie_storyline
-        self.poster_image_url=poster_image
-        self.trailer_youtube_url=trailer_youtube
+# coding=utf-8
+
+"""
+Module to display movie object, attributes and instances
+"""
+
+
+class Movie(object):
+    """
+    Class object stores movie related information
+    """
+
+    def __init__(self, movie_title, movie_storyline, movie_poster,
+            movie_trailer):
+        """
+        initialize instance of class Movie
+
+        :param movie_title: title
+        :param movie_storyline: storyline
+        :param poster_image: poster_image_url
+        :param trailer_youtube: trailer_youtube_url
+        """
+
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = movie_poster
+        self.trailer_youtube_url = movie_trailer
+
     def show_trailer(self):
+        """
+        initializing instance for opening the youtube video
+        :return: webbrowser to play trailer
+        """
         webbrowser.open(self.trailer_youtube_url)
